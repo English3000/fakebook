c9f079ef16b42330795f934c3b4f198d4096ad14
Alexander <english3000.org@gmail.com>
2017-11-21 17:44:12 -0800

    Session API

---
+++ frontend/reducers/sessionReducer.js
@@ -0,0 +1,13 @@
+import { RECEIVE_CURRENT_USER } from '../session_actions';
+
+const _nullUser = {currentUser: null};
+
+export default (state = _nullUser, action) => {
+  Object.freeze(state);
+  switch (action.type) {
+    case :
+
+    default:
+      return state
+  }
+};
---
+++ frontend/session_actions.js
@@ -0,0 +1,26 @@
+import * as SessionAPIUtil from './session_api_util';
+
+export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
+export const RECEIVE_ERRORS = 'RECEIVE_ERRORS';
+
+export const receiveCurrentUser = currentUser => ({
+  type: RECEIVE_CURRENT_USER,
+  currentUser
+});
+export const receiveErrors = errors => ({
+  type: RECEIVE_ERRORS,
+  errors
+});
+
+export const signUp = user => async (dispatch) => (
+  dispatch(receiveCurrentUser(await SessionAPIUtil.signUp(user)));
+);
+export const signIn = user => async (dispatch) => (
+  dispatch(receiveCurrentUser(await SessionAPIUtil.signIn(user)));
+);
+// export const signOut = () => async (dispatch) => (
+//   dispatch(receiveCurrentUser(await SessionAPIUtil.signOut()));
+// );
+export const signOut = () => dispatch => {
+  SessionAPIUtil.signOut().then(dispatch(receiveCurrentUser(null)));
+};
