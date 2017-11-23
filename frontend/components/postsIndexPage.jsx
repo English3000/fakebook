cc0f42cfe08c9bc4d48f402eb81f914b247a4706
Alexander <english3000.org@gmail.com>
2017-11-22 19:08:25 -0800

    ...

--- frontend/components/postsIndexPage.jsx
+++ frontend/components/postsIndexPage.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import NavContainer from './navContainer';
+// import PostFormContainer from './postFormContainer';
+import PostsIndex from './postsIndex';
 
-export default () => (
+export default () => (<div>
   <NavContainer />
-);
+  {/* <PostFormContainer /> */}
+  {/* <PostsIndex /> */}
+</div>);
--- frontend/components/userShowPage.jsx
+++ frontend/components/userShowPage.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import NavContainer from './navContainer';
+// import UserProfileContainer from './navContainer';
+// import PostFormContainer from './postFormContainer';
+import PostsIndex from './postsIndex';
 
-export default () => (
+export default () => (<div>
   <NavContainer />
-);
+  <main className='lavender-to-bottom'>
+    <div className='center-900px'>
+      {/* <UserProfileContainer /> */}
+      {/* <PostFormContainer /> */}
+      {/* <PostsIndex /> */}
+    </div>
+  </main>
+</div>);
