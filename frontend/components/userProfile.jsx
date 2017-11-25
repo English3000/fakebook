bced708e6082a7730707e7029f9fe1d42faa6a65
Alexander <english3000.org@gmail.com>
2017-11-24 19:22:19 -0800

    User Profile complete MINUS working photo-storage (look at AWS demo)

--- frontend/components/userProfile.jsx
+++ frontend/components/userProfile.jsx
@@ -35,29 +35,38 @@ export default class UserProfile extends React.Component {
     return (<div>
       <section>
         <div className='cover-photo'>
-          {users[match.params.id] && users[match.params.id].cover_photo ?
-            <img className='cover-photo' src={users[match.params.id].cover_photo}/> :
-            <div><button className='button-to-add cover-photo-button' onClick={this.select('select-cover-photo')}>+ cover photo</button>
-            <input type="file"
-                    id="select-cover-photo"
-                     onChange={this.handleSubmit('cover_photo')}/></div>}
+
+          {users[match.params.id] && users[match.params.id].cover_photo ? <div>
+            <img className='cover-photo' src={users[match.params.id].cover_photo}/>
+            <i className='fa fa-pencil-square-o fa-2x palegreen' id='cover-edit' onClick={this.select('select-profile-pic')}></i>
+          </div> : <div>
+            <button className='cover-photo-button plus-button' onClick={this.select('select-cover-photo')}>+ cover photo</button>
+          </div>}
+
+         <input type="file"
+                 id="select-cover-photo"
+                  onChange={this.handleSubmit('cover_photo')}/>
         </div>
 
         <div className='flex-bottom flex-between user-info-div'>
           <div className='flex-bottom'>
             <div className='profile-pic'>
-              {users[match.params.id] && users[match.params.id].profile_pic ?
-                <img className='profile-pic' src={users[match.params.id].profile_pic}/> :
-                <div><button className='button-to-add' onClick={this.select('select-profile-pic')}>+ profile photo</button>
-                <input type="file"
-                        id="select-profile-pic"
-                         onChange={this.handleSubmit('profile_pic')}/></div>}
+
+              {users[match.params.id] && users[match.params.id].profile_pic ? <div>
+                  <img className='profile-pic' src={users[match.params.id].profile_pic}/>
+                  <i className='fa fa-pencil-square-o fa-2x palegreen' id='profile-edit' onClick={this.select('select-profile-pic')}></i>
+              </div> : <div>
+                  <button className='plus-button' onClick={this.select('select-profile-pic')}>+ profile photo</button>
+              </div>}
+
+              <input type="file" id="select-profile-pic"
+                     onChange={this.handleSubmit('profile_pic')}/>
             </div>
             <p id='username'>{users[match.params.id] ? users[match.params.id].username : ''}</p>
           </div>
           <div>
             {this.props.currentUser - this.props.currentPage !== 0 ?
-              <p className='button-to-add'>+ friend</p> : ''}
+              <p className='plus-button friend-button'>+ friend</p> : ''}
           </div>
         </div>
       </section>
