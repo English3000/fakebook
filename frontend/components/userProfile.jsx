bb3025ebe3a6f1e1bce67fbeb6d8d4dde6dbb89c
Alexander <english3000.org@gmail.com>
2017-11-26 15:33:26 -0800

    cleaner code; Friend button better positioned

--- frontend/components/userProfile.jsx
+++ frontend/components/userProfile.jsx
@@ -62,36 +62,26 @@ export default class UserProfile extends React.Component {
                     onChange={this.handleSubmit('cover_photo')}/>
           </div>
 
-          <div className='flex-bottom flex-between user-info-div'>
-            <div className='flex-bottom'>
-              <div className='profile-pic' onClick={this.select('select-profile-pic')}>
+          <div className='flex-bottom user-info-div'>
+            <div className='profile-pic' onClick={this.select('select-profile-pic')}>
+              {user && user.profile_pic ? <div>
+                  <img className='profile-pic' src={user.profile_pic}/>
+                  <i className='fa fa-pencil-square-o fa-2x palegreen' id='profile-edit'></i>
+              </div> : <button className='plus-button'>+ profile photo</button>}
 
-                {user && user.profile_pic ? <div>
-                    <img className='profile-pic' src={user.profile_pic}/>
-                    <i className='fa fa-pencil-square-o fa-2x palegreen' id='profile-edit'></i>
-                </div> : <div>
-                    <button className='plus-button'>+ profile photo</button>
-                </div>}
-
-                <input type="file" id="select-profile-pic"
-                       onChange={this.handleSubmit('profile_pic')}/>
-              </div>
-              <div id='username'>{user && user.custom_link ?
-                <a href={user.custom_link} target='_blank'>
-                  {user.username}
-                </a> : user ? user.username : ''}&nbsp;
-                <i className='fa fa-link' onClick={() => $('.fa-link').toggleClass('visible')}>
-                  &nbsp;<form id='custom-link-form' onSubmit={this.handleSubmit('custom_link')}>
-                    <input type='url' id='custom-link-input' placeholder='http://Set-Custom-Link' defaultValue={this.state.custom_link} />
-                    <input type='submit' value='save'/>
-                  </form>
-                </i>
-
-              </div>
+              <input type="file" id="select-profile-pic" onChange={this.handleSubmit('profile_pic')}/>
             </div>
-            <div>
-              {this.props.currentUser - this.props.currentPage !== 0 ?
-                <p className='plus-button friend-button'>+ friend</p> : ''}
+
+            <div id='username'>{user && user.custom_link ?
+              <a href={user.custom_link} target='_blank'>
+                {user.username}
+              </a> : user ? user.username : ''}&nbsp;
+              <i className='fa fa-link' onClick={() => $('.fa-link').toggleClass('visible')}>
+                &nbsp;<form id='custom-link-form' onSubmit={this.handleSubmit('custom_link')}>
+                  <input type='url' id='custom-link-input' placeholder='http://Set-Custom-Link' defaultValue={this.state.custom_link} />
+                  <input type='submit' value='save'/>
+                </form>
+              </i>
             </div>
           </div>
         </section>
@@ -106,23 +96,19 @@ export default class UserProfile extends React.Component {
           </div>
 
           <div className='flex-bottom user-info-div'>
-            <div className='flex-bottom'>
-              <div className='profile-pic no-pointer'>
-                {user && user.profile_pic ? <div>
-                  <img className='profile-pic no-pointer' src={user.profile_pic}/>
-                </div> : ''}
-              </div>
-
-              <div id='username'>{user && user.custom_link ?
-                <a href={user.custom_link} target='_blank'>
-                  {user.username}
-                </a> : user ? user.username : ''}
-              </div>
+            <div className='profile-pic no-pointer'>
+              {user && user.profile_pic ? <div>
+                <img className='profile-pic no-pointer' src={user.profile_pic}/>
+              </div> : ''}
             </div>
 
-            <div>
-              <p className='plus-button friend-button'>+ friend</p>
+            <div id='username'>{user && user.custom_link ?
+              <a href={user.custom_link} target='_blank'>
+                {user.username}
+              </a> : user ? user.username : ''}
             </div>
+
+            <button className='plus-button friend-button'>+ friend</button>
           </div>
         </section>
       </div>);
