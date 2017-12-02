d51ad508cf8845a6e0f9ad19520c3fd01176b4c7
Alexander <english3000.org@gmail.com>
2017-12-01 16:00:03 -0800

    ...

--- frontend/store.js
+++ frontend/store.js
@@ -2,12 +2,15 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers/rootReducer';
 
+let middlewares = [thunk];
+
 if (process.env.NODE_ENV !== 'production') {
   const { logger } = require('redux-logger');
+  middlewares.push(logger);
 }
 
 export default (preloadedState = {}) => createStore(
   rootReducer,
   preloadedState,
-  applyMiddleware(thunk, logger)
+  applyMiddleware(...middlewares)
 );
