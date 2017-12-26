11ebcb48de7166ce2f53761ac2bc36dbdd9112b2
Alexander <english3000.org@gmail.com>
2017-12-25 16:31:45 -0800

    debugging

--- frontend/components/friendsGrid.jsx
+++ frontend/components/friendsGrid.jsx
@@ -8,7 +8,7 @@ export default class FriendsGrid extends React.Component {
       <h2 style='text-align:center'>Friends</h2>
       <div class='flex-center'>
         {users[currentUser].friend_ids.map(
-          friend_id => (<div class='friend' style={`background-image: ${users[friend_id].profile_pic} object-fit:contain`}>
+          friend_id => (<div class='friend'>
             <Link to={`/users/${friend_id}`}>{users[friend_id].username}</Link>
           </div>)
         )}
@@ -16,3 +16,5 @@ export default class FriendsGrid extends React.Component {
     </div>);
   }
 }
+
+// style={`background-image: ${users[friend_id].profile_pic} object-fit:contain`}
