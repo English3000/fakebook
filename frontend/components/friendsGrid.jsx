52286711b1b052be18f07278fc4b7edcd4667489
Alexander <english3000.org@gmail.com>
2017-12-25 16:05:55 -0800

    styling, bug resolved

--- frontend/components/friendsGrid.jsx
+++ frontend/components/friendsGrid.jsx
@@ -3,16 +3,16 @@ import { Link } from 'react-router-dom';
 
 export default class FriendsGrid extends React.Component {
   render() {
-    const { users, currentUser } = this.props.users;
-    return (<div>
-      <h1>Friends</h1>
-      {/* <div class='flex' id='friends-grid'>
+    const { users, currentUser } = this.props;
+    return (<div id='friends-grid'>
+      <h2 style='text-align:center'>Friends</h2>
+      <div class='flex-center'>
         {users[currentUser].friend_ids.map(
-          friend => (<div class='friend' style={`background-image: ${friend.profile_pic}`}>
+          friend => (<div class='friend' style={`background-image: ${friend.profile_pic} object-fit:contain`}>
             <Link to={`/users/${friend.id}`}>{friend.username}</Link>
           </div>)
         )}
-      </div> */}
+      </div>
     </div>);
   }
 }
