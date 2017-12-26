0a91c6062aa3aa910b90a6560456b6f63b7db03f
Alexander <english3000.org@gmail.com>
2017-12-26 10:59:38 -0800

    debugged; styling

--- frontend/components/friendsGrid.jsx
+++ frontend/components/friendsGrid.jsx
@@ -4,11 +4,7 @@ import { Link } from 'react-router-dom';
 export default class FriendsGrid extends React.Component {
   render() {
     const { users, pageId } = this.props;
-    console.log(users);
-    if (Object.keys(users).length < 2) {
-      return null;
-    }
-    return (<div id='friends-grid'>
+    return Object.keys(users).length < 2 ? null : (<div id='friends-grid'>
       <h3>Friends</h3>
       <div className='flex-center'>
         {users[pageId].friend_ids.map(
