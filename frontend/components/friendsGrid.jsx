dc52441b4795199c1ed194156401dd93adcf583c
Alexander <english3000.org@gmail.com>
2017-12-26 11:43:11 -0800

    ...

--- frontend/components/friendsGrid.jsx
+++ frontend/components/friendsGrid.jsx
@@ -4,17 +4,19 @@ import { Link } from 'react-router-dom';
 export default class FriendsGrid extends React.Component {
   render() {
     const { users, pageId, currentUser } = this.props;
+    let n = users[pageId].friend_ids.length;
     return Object.keys(users).length < 2 ? null : (<div id='friends-grid'>
-      {pageId - currentUser === 0 ? null : <h3 style={{marginBottom: 8}}>
-        {users[pageId].friend_ids.length} Friends</h3>}
+      {pageId - currentUser === 0 ? null : <h3 style={{fontWeight: 'bold', marginBottom: 4}}>
+        {n} Friend{n - 1 === 0 ? '' : 's'}</h3>}
       <div className='flex-center'>
         {users[pageId].friend_ids.map(
           friendId => (<div><Link className='friend' to={`/users/${friendId}`}
               style={{backgroundImage: `url(${users[friendId].profile_pic})`,
                       display: 'flex', alignItems: 'flex-end', borderRadius: '100%'}}>
             </Link>
-            <Link style={{textAlign: 'center', overflowWrap: 'break-word'}}
-                  to={`/users/${friendId}`}>{users[friendId].username}</Link></div>)
+            <Link style={{width: 70, textAlign: 'center', overflowWrap: 'break-word',
+                          marginBottom: 2, fontSize: 10}} to={`/users/${friendId}`}>
+              {users[friendId].username}</Link></div>)
         )}
       </div>
     </div>);
