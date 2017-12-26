9c2e61390cdace2aa2c3a31c016f07596d5839ed
Alexander <english3000.org@gmail.com>
2017-12-25 16:41:43 -0800

    ...

--- frontend/components/friendsGrid.jsx
+++ frontend/components/friendsGrid.jsx
@@ -5,10 +5,10 @@ export default class FriendsGrid extends React.Component {
   render() {
     const { users, currentUser } = this.props;
     return (<div id='friends-grid'>
-      <h2 style='text-align:center'>Friends</h2>
-      <div class='flex-center'>
+      <h2 style={{textAlign: 'center'}}>Friends</h2>
+      <div className='flex-center'>
         {users[currentUser].friend_ids.map(
-          friend_id => (<div class='friend'>
+          friend_id => (<div className='friend'>
             <Link to={`/users/${friend_id}`}>{friend_id}</Link>
           </div>)
         )}
