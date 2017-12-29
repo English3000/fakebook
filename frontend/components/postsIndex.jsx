189133f9a57ce75052826ecf1aa16e08d544f7af
Alexander <english3000.org@gmail.com>
2017-12-28 23:49:00 -0800

    <input> styling; positioning chat hover for UserShow

--- frontend/components/postsIndex.jsx
+++ frontend/components/postsIndex.jsx
@@ -15,16 +15,17 @@ export default class PostsIndex extends React.Component {
     return (this.props.pageLoading ? <LoadingIcon /> :
       <div className='ghostwhite-100pct' style={{display: 'flex', justifyContent: 'center'}}>
         {match.params.id ? null : <div className='hover-dark'
-          style={{marginTop: 15, marginLeft: 8, marginRight: 15, width: 202, height: 730,
-                  fontSize: 18, display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+          style={{marginTop: 15, marginLeft: 8, marginRight: 15, color: '#606060',
+                  width: 202, height: 730, fontSize: 18, display: 'flex',
+                  justifyContent: 'center', alignItems: 'center'}}>
           Links
-        </div>}
+        </div> }
         <ul style={{width: 450}}>
           {match.params.id && !users[currentUser].friend_ids.includes(parseInt(match.params.id)) ? '' :
           <div>
             <div className='px15'></div>
             <PostFormContainer errors={this.props.errors.session} />
-          </div>}
+          </div> }
           {posts && posts.all_ids.length > 0 ? posts.all_ids.map(id => {
             const post = posts.by_id[id];
 
@@ -33,16 +34,21 @@ export default class PostsIndex extends React.Component {
             }) : <div>{/* welcome message/pop up for new user */}</div>}
         </ul>
         {match.params.id ? null : <div style={{marginLeft: 15, width: 210}}>
-          <div className='hover-dark' style={{height: 350, fontSize: 18, marginTop: 15, display: 'flex',
-                                              justifyContent: 'center', alignItems: 'center'}}>
-            Trending
-          </div>
-        </div>}
-        <div className='hover-dark' style={{position: 'fixed', marginLeft: 575, marginTop: 15, fontSize: 18,
-                                            width: 210, height: 730, display: 'flex',
-                                            justifyContent: 'center', alignItems: 'center'}}>
-          Chat
-        </div>
+          <div style={{height: 350, fontSize: 18, marginTop: 15, color: '#606060',
+                       display: 'flex', justifyContent: 'center', alignItems: 'center'}}
+                       className='hover-dark'>
+            Trending</div>
+        </div> }
+        {match.params.id ? <div className='hover-dark' id='flush-top'
+          style={{position: 'fixed', marginLeft: 575, marginTop: -186, color: '#606060',
+                       fontSize: 18, width: 210, height: 926, display: 'flex',
+                       justifyContent: 'center', alignItems: 'center', borderTop: 'none'}}>
+          Chat</div> :
+        <div style={{position: 'fixed', marginLeft: 575, marginTop: 15, color: '#606060',
+                     fontSize: 18, width: 210, height: 730, display: 'flex',
+                     justifyContent: 'center', alignItems: 'center'}}
+                     className='hover-dark'>
+          Chat</div> }
       </div>);
   }
 }
