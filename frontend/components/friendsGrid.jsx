98e5feeac3cc922423479da1d7ac82ef4a957ac7
Alexander <english3000.org@gmail.com>
2018-01-01 12:09:51 -0800

    ...

--- frontend/components/friendsGrid.jsx
+++ frontend/components/friendsGrid.jsx
@@ -5,7 +5,7 @@ export default class FriendsGrid extends React.Component {
   render() {
     const { users, pageId, currentUser } = this.props;
     return Object.keys(users).length < 2 ? null : (<div id='friends-grid'>
-      {pageId - currentUser === 0 ? null : <h3 style={{fontWeight: 'bold', marginBottom: 4}}>
+      {pageId - currentUser === 0 ? null : <h3 style={{fontWeight: 'bold', marginBottom: 6}}>
         {users[pageId].friend_ids.length} Friend{users[pageId].friend_ids.length - 1 === 0 ? '' : 's'}</h3>}
       <div className='flex-center' style={{flexWrap: 'wrap'}}>
         {users[pageId].friend_ids.map(
@@ -15,7 +15,7 @@ export default class FriendsGrid extends React.Component {
                       alignItems: 'flex-end', borderRadius: '100%'}}>
             </Link>
             <div style={{width: 70, textAlign: 'center', fontSize: 12,
-                         overflowWrap: 'break-word', marginBottom: 2}}>
+                         overflowWrap: 'break-word', marginBottom: 6}}>
               <Link to={`/users/${friendId}`}>{users[friendId].username}</Link>
             </div>
           </div>)
